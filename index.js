401f7187787cde8b5c365312d16e315181bd59bb
Phillip <phillip.mittmann@gmail.com>
2020-11-02 16:27:52 -0300

    Repeticoes

--- index.js
+++ index.js
@@ -8,12 +8,31 @@ app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
 
+    var exibirMsg = true;
+
+    var produtos = [
+        {
+            nome: 'Doritos',
+            preco: 3.14
+        },
+        {
+            nome: 'Soda',
+            preco: 5
+        },
+        {
+            nome: 'Leite',
+            preco: 2
+        }
+    ]
+
     // Não é necessário extensão
     res.render("index.ejs", {
         nome: nome,
         lang: lang,
         empresa: "Teste",
-        inscritos: 8000
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos: produtos
     });
     // res.render("home.ejs");
     // res.render("principal/perfil.ejs");
