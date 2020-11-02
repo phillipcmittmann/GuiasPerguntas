084977408362ad131b9d706023e6862df3b51772
Phillip <phillip.mittmann@gmail.com>
2020-11-02 16:12:22 -0300

    Exibicao de variaveis

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const app = express();
 // Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+
     // Não é necessário extensão
-    // res.render("index.ejs");
+    res.render("index.ejs", {
+        nome: nome,
+        lang: lang,
+        empresa: "Teste",
+        inscritos: 8000
+    });
     // res.render("home.ejs");
-    res.render("principal/perfil.ejs");
+    // res.render("principal/perfil.ejs");
 });
 
 app.listen(4000, () => {
