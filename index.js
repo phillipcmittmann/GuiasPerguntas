6b7bde2eae9201d775f59098359ceb01bf40450f
Phillip <phillip.mittmann@gmail.com>
2020-11-06 12:18:41 -0300

    Basicos bootstrap

--- index.js
+++ index.js
@@ -6,38 +6,9 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-
-    var exibirMsg = true;
-
-    var produtos = [
-        {
-            nome: 'Doritos',
-            preco: 3.14
-        },
-        {
-            nome: 'Soda',
-            preco: 5
-        },
-        {
-            nome: 'Leite',
-            preco: 2
-        }
-    ]
-
+app.get("/", (req, res) => {
     // Não é necessário extensão
-    res.render("index.ejs", {
-        nome: nome,
-        lang: lang,
-        empresa: "Teste",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
-    // res.render("home.ejs");
-    // res.render("principal/perfil.ejs");
+    res.render("index");
 });
 
 app.listen(4000, () => {
