3d942a2e7ee5422675b35b514e68afbc38af43ac
Phillip <phillip.mittmann@gmail.com>
2020-11-06 12:34:58 -0300

    Utilizacao boostrap

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(express.static('public'));
 app.get("/", (req, res) => {
     // Não é necessário extensão
     res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
 });
 
 app.listen(4000, () => {
