872a1b2739512a00e6fd5e5fd05b36240dcd070a
Phillip <phillip.mittmann@gmail.com>
2020-12-03 21:49:18 -0300

    Body-parser

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     // Não é necessário extensão
@@ -14,6 +17,13 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarPergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.send(`Titulo: ${titulo} Descricao: ${descricao}`)
 });
 
 app.listen(4000, () => {
